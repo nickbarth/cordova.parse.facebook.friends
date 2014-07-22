5be97802f99b0556027dc5a65db27176e58d245f
nickbarth <nickbarth@live.com>
2014-07-22 11:36:06 -0600

    added facebook link account

--- src/ios/iOSFaceBookFriends.m
+++ src/ios/iOSFaceBookFriends.m
@@ -6,6 +6,20 @@
 
 - (void)iOSFaceBookFriends:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
+
+    PFUser *currentUser = [PFUser currentUser];
+
+    if (![PFFacebookUtils isLinkedWithUser:currentUser]) {
+        [PFFacebookUtils linkUser:currentUser permissions:nil block:^(BOOL succeeded, NSError *error) {
+            if (error) {
+                NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                [self writeJavascript:javaScript];
+            }
+        }];
+    }
+
     [[FBRequest requestForGraphPath:@"/me/invitable_friends"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
         if (error) {
             NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
