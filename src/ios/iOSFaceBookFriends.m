c729ded56e4592acdbedb08d0e0c796657be3b86
Nick Barth <nickbarth@live.com>
2014-07-22 12:07:08 -0600

    Update iOSFaceBookFriends.m

--- src/ios/iOSFaceBookFriends.m
+++ src/ios/iOSFaceBookFriends.m
@@ -8,7 +8,7 @@
     self.callbackId = command.callbackId;
 
     PFUser *currentUser = [PFUser currentUser];
-
+    
     if (![PFFacebookUtils isLinkedWithUser:currentUser]) {
         [PFFacebookUtils linkUser:currentUser permissions:nil block:^(BOOL succeeded, NSError *error) {
             if (error) {
@@ -19,9 +19,11 @@
             }
         }];
     }
-
-    [[FBRequest requestForGraphPath:@"/me/invitable_friends"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+    
+    [[FBRequest requestForGraphPath:@"/me/friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
         if (error) {
+            NSLog(@"Error: %@", error);
+            
             NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
             NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
@@ -31,11 +33,12 @@
             NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"friends\": ["];
 
             for (NSDictionary *friend in friends) {
-                result = [NSString stringWithFormat:@"%@ { \"id\": \"%@\", \"name\": \"%@\", \"picture\": \"%@\"},", 
-                    result, [friend objectForKey:@"id"], [friend objectForKey:@"name"], [friend objectForKey:@"picture"]];
+                result = [NSString stringWithFormat:@"%@ { \"id\": \"%@\", \"name\": \"%@\", \"picture\": \"%@\"},",
+                    result, [friend objectForKey:@"id"], [friend objectForKey:@"name"], [[[friend objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]];
             }
 
             result = [NSString stringWithFormat:@"%@] }", [result substringToIndex:[result length] - 1]];
+            
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
             NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
             [self writeJavascript:javaScript];
