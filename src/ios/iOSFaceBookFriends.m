72bd59c139b3fdd062ccdfd6ec844ff1e9a1fbc0
Nick Barth <nickbarth@live.com>
2014-08-03 18:53:18 -0600

    Update iOSFaceBookFriends.m

--- src/ios/iOSFaceBookFriends.m
+++ src/ios/iOSFaceBookFriends.m
@@ -6,48 +6,87 @@
 
 - (void)iOSFaceBookFriends:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
-
+    
     PFUser *currentUser = [PFUser currentUser];
     
     if (![PFFacebookUtils isLinkedWithUser:currentUser]) {
         [PFFacebookUtils linkUser:currentUser permissions:nil block:^(BOOL succeeded, NSError *error) {
             if (error) {
-                NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
+                NSString *result = [NSString stringWithFormat:@"{ \"error\": \"Invalid Session Token\" }"];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                [self writeJavascript:javaScript];
+            } else {
+                
+                
+                
+                [[FBRequest requestForGraphPath:@"/me/friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                    if (error) {
+                        NSLog(@"Error: %@", error);
+                        
+                        NSString *result = [NSString stringWithFormat:@"{ \"error\": \"Invalid Session Token\" }"];
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                        [self writeJavascript:javaScript];
+                    } else {
+                        NSArray *friends = [result objectForKey:@"data"];
+                        NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"friends\": ["];
+                        
+                        //int counter = 0;
+                        for (NSDictionary *friend in friends) {
+                            //counter = counter + 1;
+                            result = [NSString stringWithFormat:@"%@ { \"id\": \"%@\", \"name\": \"%@\", \"picture\": \"%@\"},",
+                                      result, [friend objectForKey:@"id"], [friend objectForKey:@"name"], [[[friend objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]];
+                            //if (counter > 5) break;
+                        }
+                        
+                        if ([friends count] > 1) {
+                            result = [NSString stringWithFormat:@"%@] }", [result substringToIndex:[result length] - 1]];
+                        } else {
+                            result = [NSString stringWithFormat:@"%@] }", result];
+                        }
+                        
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                        [self writeJavascript:javaScript];
+                    }
+                }];
+            }
+        }];
+    } else {
+        
+        [[FBRequest requestForGraphPath:@"/me/friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+            if (error) {
+                NSLog(@"Error: %@", error);
+                
+                NSString *result = [NSString stringWithFormat:@"{ \"error\": \"Invalid Session Token\" }"];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                [self writeJavascript:javaScript];
+            } else {
+                NSArray *friends = [result objectForKey:@"data"];
+                NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"friends\": ["];
+                
+                //int counter = 0;
+                for (NSDictionary *friend in friends) {
+                    //counter = counter + 1;
+                    result = [NSString stringWithFormat:@"%@ { \"id\": \"%@\", \"name\": \"%@\", \"picture\": \"%@\"},",
+                              result, [friend objectForKey:@"id"], [friend objectForKey:@"name"], [[[friend objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]];
+                    //if (counter > 5) break;
+                }
+                
+                if ([friends count] > 1) {
+                    result = [NSString stringWithFormat:@"%@] }", [result substringToIndex:[result length] - 1]];
+                } else {
+                    result = [NSString stringWithFormat:@"%@] }", result];
+                }
+                
                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
                 NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
                 [self writeJavascript:javaScript];
             }
         }];
     }
-    
-    [[FBRequest requestForGraphPath:@"/me/friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
-        if (error) {
-            NSLog(@"Error: %@", error);
-            
-            NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
-        } else {
-            NSArray *friends = [result objectForKey:@"data"];
-            NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"friends\": ["];
-
-            for (NSDictionary *friend in friends) {
-                result = [NSString stringWithFormat:@"%@ { \"id\": \"%@\", \"name\": \"%@\", \"picture\": \"%@\"},",
-                    result, [friend objectForKey:@"id"], [friend objectForKey:@"name"], [[[friend objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]];
-            }
-
-            if ([friends count] > 1) {
-                result = [NSString stringWithFormat:@"%@] }", [result substringToIndex:[result length] - 1]];
-            } else {
-                result = [NSString stringWithFormat:@"%@] }", result];
-            }
-            
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-            [self writeJavascript:javaScript];
-        }
-    }];
 }
 
 @end
