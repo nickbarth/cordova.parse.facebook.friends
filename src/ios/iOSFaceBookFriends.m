4197819de9e3dd91997e3b9fd13036da2d45d7c7
Nick Barth <nickbarth@live.com>
2014-08-30 11:27:06 -0600

    Update iOSFaceBookFriends.m

--- src/ios/iOSFaceBookFriends.m
+++ src/ios/iOSFaceBookFriends.m
@@ -6,26 +6,24 @@
 
 - (void)iOSFaceBookFriends:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
-    
-                
-                
-                [[FBRequest requestForGraphPath:@"/me/invitable_friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
-                    if (error) {
-                        NSLog(@"Error: %@", error);
-                        
-                        NSString *result = [NSString stringWithFormat:@"{ \"error\": \"Invalid Session Token\" }"];
-                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-                        [self writeJavascript:javaScript];
-                    } else {
-                        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:NSJSONWritingPrettyPrinted error:&writeError];
-                        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-                        
-                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
-                        NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
-                        [self writeJavascript:javaScript];
-                    }
-                }];
+    [[FBRequest requestForGraphPath:@"/me/invitable_friends?fields=id,name,picture"] startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+            
+            NSString *result = [NSString stringWithFormat:@"{ \"error\": \"Invalid Session Token\" }"];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+            [self writeJavascript:javaScript];
+        } else {
+            NSError *writeError = nil;
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:NSJSONWritingPrettyPrinted error:&writeError];
+            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
+            NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+            [self writeJavascript:javaScript];
+        }
+    }];
 }
 
 @end
