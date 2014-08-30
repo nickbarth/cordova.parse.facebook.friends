716fdb439736432c1e1c5e60f34bfb85e661dc7d
Nick Barth <nickbarth@live.com>
2014-08-30 11:11:15 -0600

    Update iOSFaceBookFriends.js

--- www/iOSFaceBookFriends.js
+++ www/iOSFaceBookFriends.js
@@ -4,11 +4,21 @@ var exec = require('cordova/exec');
 var iOSFaceBookFriends = function(callback) {
   var iOSFaceBookFriendsReturn = function (json) {
     var result = JSON.parse(json);
+    var friends = null;
+    
+    if (result.error) {
+      return callback(result.error);
+    }
+    
+    if (!result.data) {
+      return callback('could not connect to facebook');  
+    }
 
-    if (!result.data)
-      return callback('unable to connect to facebook');
-
-    return callback(null, data.data);
+    friends = result.data.map(function (friend) {
+      return { id: friend.id, name: friend.name, picture: friend.picture.data.url };
+    });
+    
+    return callback(null, friends);
   }
 
   exec(iOSFaceBookFriendsReturn, iOSFaceBookFriendsReturn, 'iOSFaceBookFriends', 'iOSFaceBookFriends', []);
